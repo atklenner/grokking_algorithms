9849c3680024e17680d2b7ff2a503e2386e57ecf
Andrew Klenner <atklenner@gmail.com>
2022-08-09 16:35:47 -0700

    actually prove it works

--- BFS.py
+++ BFS.py
@@ -1,9 +1,17 @@
 from collections import deque
 
 graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
 
 def person_we_are_looking_for(person):
-    pass
+    return person[-1] == "m"
 
 def search(starting_node):
     search_queue = deque()
@@ -18,3 +26,5 @@ def search(starting_node):
                 search_queue += graph[person]
                 searched.append(person)
     return False
+
+print(search("you"))
