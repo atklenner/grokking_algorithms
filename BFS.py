0911c6a28c4fcc79d7ab54738623e4361ae15872
Andrew Klenner <atklenner@gmail.com>
2022-08-08 23:22:19 -0700

    finish chapter 6

---
+++ BFS.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+graph = {}
+
+def person_we_are_looking_for(person):
+    pass
+
+def search(starting_node):
+    search_queue = deque()
+    search_queue += graph[starting_node]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_we_are_looking_for(person):
+                return True
+        else:
+            search_queue += graph[person]
+            searched.append(person)
+    return False
